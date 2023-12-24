9d09af78902f976f64bc718122a6f8894ac37b50
k.enomoto <red.ke.5084@gmail.com>
2023-12-24 23:33:56 +0900

    [add]カードのデザイン調整とスライドダウンメニューの追加

--- src/views/RecipiesView.vue
+++ src/views/RecipiesView.vue
@@ -1,26 +1,67 @@
 <template>
-  <draggable tag="ul">
-    <li v-for="(item, index) in testlist" :key="item.name">
-      {{ item.name }}-{{ index }}
+  <draggable>
+    <li
+      style="list-style: none"
+      v-for="(item, index) in testlist"
+      :key="item.name"
+      @dragstart="dragStart(index)"
+      @dragenter="dragEnter(index)"
+      @click="item.drawer = !item.drawer"
+    >
+      <!-- 
+          dragのデザイン作り
+          ★ ドラッグをしたときの残像、カーソルを当てた位置ではなくて右端が支点となってしまう・・・
+            v-cardでなくdivだけだと問題ないのだけど…
+          ★ 
+        -->
+      <v-card>
+        <v-card-title>title-{{ item.name }}</v-card-title>
+        <v-card-subtitle
+          ><starrating
+            v-model="item.rating"
+            :increment="1"
+            :rating="1"
+            :max-rating="5"
+            :read-only="true"
+            :star-size="15"
+            :show-rating="false"
+          />
+        </v-card-subtitle>
+        <slideupdown :active="item.drawer">
+          <v-card-text>テキストだよ</v-card-text>
+        </slideupdown>
+      </v-card>
     </li>
   </draggable>
 </template>
 <script>
+import starrating from "vue-star-rating";
+import slideupdown from "vue-slide-up-down";
 import draggable from "vuedraggable";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default {
   data() {
     return {
       uid: "",
+      dragIndex: null,
       testlist: [
         {
           name: "卵焼き",
+          rating: "4",
+          drawer: false,
+          dragging: false,
         },
         {
           name: "そぼろご飯",
+          rating: "5",
+          drawer: false,
+          dragging: false,
         },
         {
           name: "ハンバーグ",
+          rating: "3",
+          drawer: false,
+          dragging: false,
         },
       ],
     };
@@ -37,7 +78,10 @@ export default {
   },
   components: {
     draggable,
+    starrating,
+    slideupdown,
   },
+
   created() {
     if (this.isLogin) {
       this.uid = this.userInfo.uid;
@@ -53,6 +97,18 @@ export default {
         }
       });
     }
+  },
+  methods: {
+    dragStart(index) {
+      this.dragIndex = index;
+    },
+    dragEnter(index) {
+      if (this.dragIndex === index) return;
+      const deleteElement = this.testlist.splice(this.dragIndex, 1)[0];
+      this.testlist.splice(index, 0, deleteElement);
+      this.dragIndex = index;
+      console.log(this.testlist[0]);
+    },
   },
   // ★mouted() で取得した uid を用いてレシピ一覧を取得する
   // 1件もなければ「データは存在しません」と画面に表記する
